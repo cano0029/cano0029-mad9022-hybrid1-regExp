2814d9bcc4ceca83d414791a72321062b26a9be6
Johanna Cano <cano0029@algonquinlive.com>
2021-02-02 19:46:11 -0500

    Add function that will validate the whole form when submit is clicked

--- js/valid.js
+++ js/valid.js
@@ -14,20 +14,35 @@ function init(ev) {
 function validateForm(ev) {
   //validate the whole form
   ev.preventDefault();
-  //validateName();
-  // validateEmail();
-  // validateAddress();
-  // validateCity();
-  // validateProv();
-  // validatePostal();
-  console.log('cannot submit');
+
+  let fullName = document.getElementById('fullname').value;
+  let email = document.getElementById('email').value;
+  let address = document.getElementById('address').value;
+  let city = document.getElementById('city').value;
+  let province = document.getElementById('prov').value;
+  let postal = document.getElementById('post').value;
+  
+    validateFullName(fullName);
+    validateEmail(email);
+    validateAddress(address);
+    validateCity(city);
+    validateProvince(province);
+    validatePostal(postal);
+    console.log('HEEY');
 }
 
 function validateFullName(ev) {
   let regExp = /^[a-zA-Z]*( |-)[a-zA-Z]*( |-)*[a-zA-Z]*$/g;
-  let fullName = ev.target.value;
+  // let fullName = ev.target.value;
   let error = document.querySelector('.error-name');
   
+
+  if (typeof ev === 'string') {
+    var fullName = ev;
+  } else {
+    var fullName = ev.target.value;
+  }
+
   if(regExp.test(fullName)){
     error.innerHTML ='';
   }else {
@@ -36,9 +51,15 @@ function validateFullName(ev) {
 }
 
 function validateEmail(ev) {
-  let regExp = /^[a-zA-Z0-9\.!#$%&'*+=?^_`{|}~-]*@[a-zA-Z0-9-]*(\.[a-zA-Z]{2,4})?$/g;
-  let email = ev.target.value;
+  let regExp = /^[a-zA-Z0-9\.!#$%&'*+=?^_`{|}~-]*@[a-zA-Z0-9-]*\.[a-zA-Z]{2,4}$/g;
+  // let email = ev.target.value;
   let error = document.querySelector('.error-email');
+
+  if (typeof ev === 'string') {
+    var email = ev;
+  } else {
+    var email = ev.target.value;
+  }
 
   if(regExp.test(email)){
     error.innerHTML ='';
@@ -49,8 +70,14 @@ function validateEmail(ev) {
 
 function validateAddress(ev) {
   let regExp = /^\d{1,7}( |-)?([a-zA-Z]|\d)*( [a-zA-Z-]+\.?){1,4}(Rd|Lane|Cres|St|Ave|Circle|)\.?/g;
-  let address = ev.target.value;
+  // let address = ev.target.value;
   let error = document.querySelector('.error-address');
+
+  if (typeof ev === 'string') {
+    var address = ev;
+  } else {
+    var address = ev.target.value;
+  }
 
   if(regExp.test(address)){
     error.innerHTML ='';
@@ -61,8 +88,14 @@ function validateAddress(ev) {
 
 function validateCity(ev) {
   let regExp = /^[a-zA-Z]{2,}([ |-][a-zA-Z]+)*$/g
-  let city = ev.target.value;
+  // let city = ev.target.value;
   let error = document.querySelector('.error-city');
+
+  if (typeof ev === 'string') {
+    var city = ev;
+  } else {
+    var city = ev.target.value;
+  }
 
   if(regExp.test(city)){
     error.innerHTML ='';
@@ -72,9 +105,15 @@ function validateCity(ev) {
 }
 
 function validateProvince(ev) {
-  let province = ev.target.value;
+  // let province = ev.target.value;
   let error = document.querySelector('.error-prov');
   
+  if (typeof ev === 'string') {
+    var province = ev;
+  } else {
+    var province = ev.target.value;
+  }
+
   if(province === "selectOption"){
     error.innerHTML= 'Invalid selection';
   }else {
@@ -84,10 +123,16 @@ function validateProvince(ev) {
 
 function validatePostal(ev) {
   let regExp = /^[a-zA-Z]\d[a-zA-Z]( |-)?\d[a-zA-Z]\d$/g
-  let post = ev.target.value;
+  // let postal = ev.target.value;
   let error = document.querySelector('.error-post');
 
-  if(regExp.test(post)){
+  if (typeof ev === 'string') {
+    var postal = ev;
+  } else {
+    var postal = ev.target.value;
+  }
+
+  if(regExp.test(postal)){
     error.innerHTML ='';
   }else {
     error.innerHTML ='Invalid postal code';
